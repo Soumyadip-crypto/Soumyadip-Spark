124e2dcf2165547ba0fd8d924f83074e0b32bf2e
Soumyadip-crypto <70897396+Soumyadip-crypto@users.noreply.github.com>
2021-12-19 11:32:45 +0530

    Add files via upload

---
+++ bbsn/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid" >
+      
+            <div class="row intro py-1" style="background-color : #5de1e0; border:3px solid white;">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h1><b>ONLINE</b></h1>
+                  <h1><b>BANKING SYSTEM</b></h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+                <img src="https://webstockreview.net/images250_/bank-clipart-1.png" class="img-fluid pt-2">
+              </div>
+            </div>
+
+      
+            <div class="row activity text-center" style="background-color:#838996;" >
+                  <div class="col-md act">
+                   
+                    <img src="https://sites.google.com/site/siddharthcommercesite/_/rsrc/1293473435816/barter-system/barter2.jpg?height=400&width=350">
+                    
+                    <br>
+                    <br>
+                    <br>
+                    
+                    <a href="transfermoney.php"> <button type="button" class="btn" style="background-color  : #2785C4;"><b>Transfer Money</b></button></a>
+                    <br>
+                    <br>
+                    <br>
+                  </div>
+                  <div class="col-md act">
+                    
+                    <img src="https://www.officeclipart.com/office_clipart_images/making_money_with_your_computer__laptop_computer_with_a_sack_of_cash_0521-1004-3015-4507_SMU.jpg?height=400&width=350" class="img-fluid">
+                   
+                    <br>
+                    <br>
+                    <a href="transactionhistory.php"> <button type="button" class="btn" style="background-color  : #2785C4;"><b>Transaction History</b></button></a>
+                    <br>
+                  </div>
+            </div>
+      
+      <footer class="text-center mt-5 py-2" style="background-color:#838996;">
+      <p>&copy 2021.<b>ROYAL BANK</b> <br> Banking Foundation</p>
+      </footer>
+    </div>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
